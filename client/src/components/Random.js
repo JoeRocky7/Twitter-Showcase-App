40e3aa0f505c3a4922cba73a6ebd13f2480e7c94
Joe Mittiga <joemittiga7@hotmail.com>
2021-03-14 11:54:40 -0400

    fixed double tweet

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -27,27 +27,27 @@ const Random = () => {
             <div class="random-container">
                 <button id="random-button" name="NASA" onClick={handleClick}>Get NASA Tweet!
                     <div>
-                        <img name="NASA" onClick={handleClick} class="nasa-image" src="https://yt3.ggpht.com/ytc/AAUvwnhTEa6kEyqgSvL3wMaMbUDZtyHvhXehJppDCeOGUCI=s900-c-k-c0x00ffffff-no-rj" />
+                        <img name="NASA" class="nasa-image" src="https://yt3.ggpht.com/ytc/AAUvwnhTEa6kEyqgSvL3wMaMbUDZtyHvhXehJppDCeOGUCI=s900-c-k-c0x00ffffff-no-rj" />
                     </div>
                 </button>
                 <button id="random-button" name="Spacex" onClick={handleClick}>Get SpaceX Tweet!
                     <div>
-                        <img name="Spacex" onClick={handleClick} class="spacex-image" src="https://i.pinimg.com/originals/9a/21/4b/9a214b68fc4146d02a5b41882e79987c.jpg" />
+                        <img name="Spacex" class="spacex-image" src="https://i.pinimg.com/originals/9a/21/4b/9a214b68fc4146d02a5b41882e79987c.jpg" />
                     </div>
                 </button>
                 <button id="random-button" name="elonmusk" onClick={handleClick}>Get Elon Musk Tweet!
                     <div>
-                        <img name="elonmusk" onClick={handleClick} class="musk-image" src="https://ih1.redbubble.net/image.1226836687.3477/st,small,507x507-pad,600x600,f8f8f8.jpg" />
+                        <img name="elonmusk" class="musk-image" src="https://ih1.redbubble.net/image.1226836687.3477/st,small,507x507-pad,600x600,f8f8f8.jpg" />
                     </div>
                 </button>
                 <button id="random-button" class="random-buttons-2" name="whitegavriel" onClick={handleClick}>Get Windows 96 Tweet!
                     <div>
-                        <img name="whitegavriel" onClick={handleClick} class="windows96-image" src="https://f4.bcbits.com/img/a0122641443_10.jpg" />
+                        <img name="whitegavriel" class="windows96-image" src="https://f4.bcbits.com/img/a0122641443_10.jpg" />
                     </div>
                 </button>
                 <button id="random-button" class="random-buttons-2" name="DanMasonVapor" onClick={handleClick}>Get Dan Mason Tweet!
                     <div>
-                        <img name="DanMasonVapor" onClick={handleClick} class="mason-image" src="https://f4.bcbits.com/img/a1494578543_5.jpg" />
+                        <img name="DanMasonVapor" class="mason-image" src="https://f4.bcbits.com/img/a1494578543_5.jpg" />
                     </div>
                 </button>
             </div>
